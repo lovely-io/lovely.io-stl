b175dfdb332583dcf5aac9c26e1139930a6c2e41
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2011-04-08 12:32:06 +0400

    Brushing up the list_test

--- test/core/list_test.js
+++ test/core/list_test.js
@@ -5,36 +5,30 @@
  */
 require('../test_helper');
 
-var List = LeftJS.List;
+var List  = LeftJS.List;
+var array = [1,2,3,4,5];
+var list  = new List(array);
 
 describe("List", {
   'constructor': {
-    topic: function() {
-      return new List(this.array = [1,2,3,4,5]);
-    },
+    topic: list,
 
     'should link the original iterable by a reference': function(list) {
-      assert.same (list._, this.array);
+      assert.same (list._, array);
     }
   },
 
   '#first': {
-    topic: function() {
-      return this.list = new List([1,2,3,4,5]);
-    },
-
     '\b()': {
-      topic: function(list) {
-        return list.first();
-      },
+      topic: list.first(),
 
       'should return the first item on the list': function(item) {
-        assert.same(item, this.list._[0]);
+        assert.same(item, list._[0]);
       }
     },
 
     '\b(callback)': {
-      topic: function(list) {
+      topic: function() {
         return list.first(function(item) { return item % 2 == 0; });
       },
 
@@ -45,22 +39,16 @@ describe("List", {
   },
 
   '#last': {
-    topic: function() {
-      return this.list = new List([1,2,3,4,5]);
-    },
-
     '\b()': {
-      topic: function(list) {
-        return list.last();
-      },
+      topic: list.last(),
 
       'should return the last item on the list': function(item) {
-        assert.same(item, this.list._[this.list._.length - 1]);
+        assert.same(item, list._[list._.length - 1]);
       }
     },
 
     '\b(callback)': {
-      topic: function(list) {
+      topic: function() {
         return list.last(function(item) { return item % 2 == 0; });
       },
 
@@ -71,63 +59,46 @@ describe("List", {
   },
 
   '#size()': {
-    topic: function() {
-      this.list  = new List([1,2,3,4,5]);
-      return this.list.size();
-    },
+    topic: list.size(),
 
     'should return the list size': function(size) {
-      assert.same(size, this.list._.length);
+      assert.same(size, list._.length);
     }
   },
 
   '#item()': {
-    topic: function() {
-      this.list  = new List([1,2,3,4,5]);
-      this.index = 2;
-      return this.list.item(this.index);
-    },
+    topic: list.item(2),
 
     'should return an item at that index': function(item) {
-      assert.same(item, this.list._[this.index]);
+      assert.same(item, list._[2]);
     }
   },
 
   '#indexOf': {
-    topic: function() {
-      this.list = new List([1,2,3,4,5]);
-      this.item = 2;
-      return this.list.indexOf(this.item);
-    },
+    topic: list.indexOf(2),
 
     'should return left index for the item': function(index) {
-      assert.equal(index, this.list._.indexOf(this.item));
+      assert.equal(index, list._.indexOf(2));
     }
   },
 
   '#lastIndexOf': {
-    topic: function() {
-      this.list = new List([1,2,3,4,5]);
-      this.item = 2;
-      return this.list.lastIndexOf(this.item);
-    },
+    topic: list.lastIndexOf(2),
 
     'should return the right index for the item': function(index) {
-      assert.equal(index, this.list._.lastIndexOf(this.item));
+      assert.equal(index, list._.lastIndexOf(2));
     }
   },
 
   '#each(callback)': {
     topic: function() {
-      var items   = this.items   = [];
-      var indexes = this.indexes = [];
+      this.items   = [];
+      this.indexes = [];
 
-      this.list = new List([1,2,3,4,5]);
-
-      return this.list.each(function(item, index) {
-        items.push(item);
-        indexes.push(index);
-      });
+      return list.each(function(item, index) {
+        this.items.push(item);
+        this.indexes.push(index);
+      }, this);
     },
 
     'should get through all the items on the list': function(items) {
@@ -138,51 +109,39 @@ describe("List", {
       assert.deepEqual (this.indexes, [0,1,2,3,4]);
     },
 
-    'should return the list object back': function(list) {
-      assert.same (list, this.list);
+    'should return the list object back': function(result) {
+      assert.same (result, list);
     }
   },
 
   '#map(callback)': {
-    topic: function() {
-      this.original = new List([1,2,3,4,5]);
-
-      return this.original.map(function(item) {
-        return item * 2;
-      })
-    },
+    topic: list.map(function(item) {
+      return item * 2;
+    }),
 
     'should make a new list': ensure_new_list,
 
-    'should pack all the mapping results': function(result) {
-      assert.deepEqual      (result._, [2,4,6,8,10]);
+    'should pack all the mapping results': function(list) {
+      assert.deepEqual (list._, [2,4,6,8,10]);
     }
   },
 
   '#filter(callback)': {
-    topic: function() {
-      this.original = new List([1,2,3,4,5]);
-
-      return this.original.filter(function(item) {
-        return item % 2;
-      })
-    },
+    topic: list.filter(function(item) {
+      return item % 2;
+    }),
 
     'should create a new List': ensure_new_list,
 
-    'should pack it with filtered data': function(result) {
-      assert.deepEqual      (result._, [1,3,5]);
+    'should pack it with filtered data': function(list) {
+      assert.deepEqual (list._, [1,3,5]);
     }
   },
 
   '#reject(callback)': {
-    topic: function() {
-      this.original = new List([1,2,3,4,5]);
-
-      return this.original.reject(function(item) {
-        return item % 2;
-      });
-    },
+    topic: list.reject(function(item) {
+      return item % 2;
+    }),
 
     'should create a new List': ensure_new_list,
 
@@ -192,11 +151,7 @@ describe("List", {
   },
 
   '#without(a,b,c)': {
-    topic: function() {
-      this.original = new List([1,2,3,4,5]);
-
-      return this.original.without(1,2,4);
-    },
+    topic: list.without(1,2,4),
 
     'should create a new List': ensure_new_list,
 
@@ -211,7 +166,10 @@ describe("List", {
       return this.original.compact();
     },
 
-    'should create a new List': ensure_new_list,
+    'should create a new List': function(list) {
+      assert.instanceOf (list, List);
+      assert.notSame    (list, this.original);
+    },
 
     'should filter out nulls and undefineds': function(list) {
       assert.deepEqual (list._, ['', 0, 1]);
@@ -219,35 +177,29 @@ describe("List", {
   },
 
   '#toArray()': {
-    topic: function() {
-      this.list = new List([1,2,3,4,5]);
-      return this.list.toArray();
-    },
+    topic: list.toArray(),
 
     'should make an array out of the list': function(array) {
       assert.isArray (array);
     },
 
     'should feed it with the original data': function(array) {
-      assert.deepEqual (array, this.list._);
+      assert.deepEqual (array, list._);
     },
 
     'should make a clone of the list not refer it by a link': function(array) {
-      assert.notSame (array, this.list._);
+      assert.notSame (array, list._);
     }
   },
 
   '#clone()': {
-    topic: function() {
-      this.original = new List([1,2,3,4,5]);
-      return this.original.clone();
-    },
+    topic: list.clone(),
 
     'should make a new list': ensure_new_list,
 
-    'should clone the data': function(list) {
-      assert.deepEqual (list._, this.original._);
-      assert.notSame   (list._, this.original._);
+    'should clone the data': function(result) {
+      assert.deepEqual (result._, list._);
+      assert.notSame   (result._, list._);
     }
   }
 }, module);
@@ -258,7 +210,7 @@ describe("List", {
  * @param {List} list
  * @return void
  */
-function ensure_new_list(list) {
-  assert.instanceOf (list, List);
-  assert.notSame    (list, this.original);
+function ensure_new_list(object) {
+  assert.instanceOf (object, List);
+  assert.notSame    (object, list);
 }
